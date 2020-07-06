e394835946e7e68704c8eb5da2c461535e75f7eb
Moknine <moknial01@JV1.AD.JVGAS.COM>
2020-07-06 15:36:16 +0200

    Updates

---
+++ precache-manifest.27854f2d250613b7b7ea20e03e0ab239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4d2b2b63cd03d3e9eaa3763b016743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3eb964900009538aeb18",
+    "url": "/static/css/main.c9f4ddf8.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb964900009538aeb18",
+    "url": "/static/js/main.855f831e.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
