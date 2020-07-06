613a9073b065c76fbb6a7781eeabac4de841eb12
Moknine <moknial01@JV1.AD.JVGAS.COM>
2020-07-06 15:20:38 +0200

    errors handler

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react'
+
+class ErrorBoundry extends Component {
+    constructor(porps) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info) {
+
+        this.setState({ hasError: true })
+    }
+
+    render() {
+        if (this.state.hasError){
+            return <h1>Error: this element fale to render </h1>
+        }
+
+        return this.props.children
+    }
+
+}
+
+export default ErrorBoundry;
